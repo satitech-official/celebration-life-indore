72c7baa69eb477cb963a5884312c98540dc74c12
SatiTech <www.satitechinfo@gmail.com>
2026-07-24 13:59:05 +0530

    Configure static GitHub Pages robots metadata

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,13 @@
 import type { MetadataRoute } from "next";
-export default function robots():MetadataRoute.Robots{return {rules:{userAgent:"*",allow:"/"},sitemap:"https://celebrate-life-indore.example/sitemap.xml"}}
+
+export const dynamic = "force-static";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/"
+    },
+    sitemap: "https://satitech-official.github.io/celebration-life-indore/sitemap.xml"
+  };
+}
